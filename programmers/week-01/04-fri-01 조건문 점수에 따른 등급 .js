91635e66a94b68b13a85141cf69ff2b8e174275c
Soheebrighton <0203so@naver.com>
2021-11-05 07:32:20 +0900

    5일차

---
+++ programmers/week-01/04-fri-01 조건문 점수에 따른 등급 .js
@@ -0,0 +1,23 @@
+function grade(score) {
+  if (score >= 90 && score <= 100) {
+    return "A";
+  } else if (score >= 80 && score <= 89) {
+    return "B";
+  } else if (score >= 70 && score <= 79) {
+    return "C";
+  } else if (score >= 60 && score <= 69) {
+    return "D";
+  } else if (score <= 59) {
+    return "F";
+  } else if (score > 100 || score < 0) {
+    return "잘못된 점수입니다";
+  }
+}
+
+grade(105); // "잘못된 점수입니다"
+grade(-10); // "잘못된 점수입니다"
+grade(97); // "A"
+grade(86); // "B"
+grade(75); // "C"
+grade(66); // "D"
+grade(52); // "F"
